9bd7cbabe48975ae10f40c615c3df46dbf220384
gabrielewanzeler <gabrielewanzeler000@gmail.com>
2024-02-06 18:05:36 -0300

    add suggestion

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,6 @@
-
 document.addEventListener("DOMContentLoaded", function() {
 
 let chave = "b462b1c67156f6a825b1feb3a543b53e"
-
 
 function colocarNaTela(dados){
     console.log(dados)
@@ -10,16 +8,14 @@ function colocarNaTela(dados){
     document.querySelector(".temp").innerHTML = "Temperatura: " + Math.floor(dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-    document.querySelector(".valor-umidade").innerHTML = "Umidade atual: " + dados.main.humidity + "%";
-    document.querySelector(".valor-vento").innerHTML ="Velocidade do vento " + dados.wind.speed + "m/s"// Exibindo a velocidade do vento
-
+    document.querySelector(".valor-umidade").innerHTML = "Umidade: " + dados.main.humidity + " %";
+    document.querySelector(".valor-vento").innerHTML ="Velocidade do vento " + dados.wind.speed + " m/s"// Exibindo a velocidade do vento
     document.querySelector(".icone").style.display = "inline-block";
-
     document.querySelector(".input-cidade").value = "";
-    
 }
 
 async function buscarCidade(cidade) {
+
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
             cidade +
             "&appid=" +
@@ -32,6 +28,7 @@ async function buscarCidade(cidade) {
             console.error('Erro ao buscar cidade:', error);
             return null;
         });
+    
 
     if (dados && dados.cod === 200) {
         colocarNaTela(dados);
@@ -45,16 +42,20 @@ function exibirMensagemErro() {
     document.querySelector(".mensagem-erro").style.display = "block";
     document.querySelector(".input-cidade").style.display ="inline-block";
     document.getElementById("btnBuscarCidade").style.display = "inline-block";
+    document.getElementById("loading-message").style.display = "none";
+    document.querySelector(".suggestion").style.display = "none";
 }
 
 function ocultarMensagemErro() {
     document.querySelector(".mensagem-erro").style.display = "none";
+    document.getElementById("loading-message").style.display = "none";
+    document.querySelector(".suggestion").style.display = "none";
 }
 
 document.getElementById("btnBuscarCidade").addEventListener("click", function() {
     let cidade = document.querySelector(".input-cidade").value;
     buscarCidade(cidade);
-    ocultarElementos(); // Ocultar os elementos de entrada ao buscar uma cidade
+    ocultarElementos();
 });
 
 
@@ -63,7 +64,7 @@ document.querySelectorAll("#suggestions button").forEach(function(button) {
     button.addEventListener("click", function() {
         let cityName = button.textContent;
         buscarCidade(cityName);
-        ocultarElementos(); // Ocultar os elementos de entrada ao clicar em uma sugestão
+        ocultarElementos(); 
     });
 });
 
@@ -73,5 +74,7 @@ function ocultarElementos() {
     document.getElementById("btnBuscarCidade").style.display = "none";
     document.getElementById("suggestions").style.display = "none";
     document.getElementById("confira").style.display = "none";
+    document.getElementById("loading-message").style.display = "inline-block";
 }
+
 });
